16b4a8e90a4b719569519df89757d50eacf42693
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-07-21 09:45:56 +0100

    CHORE[Add third-party dependencies to django installed apps]

--- e_comm/settings.py
+++ e_comm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-qp!r3d@sil*q(e%_7!n$8c!scoqalz^cjvozr36t*sgtpnsunx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party apps
+    "rest_framework",
+    "corsheaders",
+    # local apps
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "e_comm.urls"
