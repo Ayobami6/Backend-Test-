0c02dc07e62143043da1331bc4fbd7fece997f31
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-07-21 09:36:04 +0100

    CHORE[Initial django project setup]

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,12 @@
 """Create and manage app models and methods."""
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
 # Create your models here.
 
 
@@ -12,7 +16,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create_user method creates and saves new user objects."""
         if not email:
-            raise ValueError('User must have valid email address')
+            raise ValueError("User must have valid email address")
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
@@ -39,4 +43,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
